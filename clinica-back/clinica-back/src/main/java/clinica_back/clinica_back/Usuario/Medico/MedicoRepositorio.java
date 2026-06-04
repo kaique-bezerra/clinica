ea9775ec62cf78ea8fb087654239aff7dad7d48e
kaique-bezerra <bezerrak114@gmail.com>
2026-06-04 18:41:45 -0300

    adcionando ima parte das pastas mais o foco e o git ignore

---
+++ clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/Usuario/Medico/MedicoRepositorio.java
@@ -0,0 +1,4 @@
+package clinica_back.clinica_back.Usuario.Medico;
+
+public interface MedicoRepositorio {
+}
---
+++ clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/Usuario/Recepcionista/RecepcionistaEntidade.java
@@ -0,0 +1,4 @@
+package clinica_back.clinica_back.Usuario.Recepcionista;
+
+public class RecepcionistaEntidade {
+}
