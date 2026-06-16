64edfe7756b7d72567d72021ee99663b9c19391a
jeh8766 <jeh87662@gmail.com>
2026-06-16 17:03:38 -0300

    Adicionado o método e rota de atualizarEndereco em usuario

--- clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/features/Usuario/UsuarioController.java
+++ clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/features/Usuario/UsuarioController.java
@@ -3,9 +3,13 @@ package clinica_back.clinica_back.features.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import clinica_back.clinica_back.features.Usuario.Endereco.dto.EnderecoRequestDTO;
+import clinica_back.clinica_back.features.Usuario.Endereco.dto.EnderecoResponseDTO;
 import clinica_back.clinica_back.features.Usuario.dto.UsuarioResponseDTO;
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +23,11 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+
+    @PutMapping("/{idUsuario}/atualizarEndereco")
+    public ResponseEntity<EnderecoResponseDTO> atualizarEndereco(@PathVariable Long idUsuario,
+            @RequestBody EnderecoRequestDTO endereco) {
+        return ResponseEntity.ok(usuarioService.atualizarEndereco(idUsuario, endereco));
     }
 }
--- clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/features/Usuario/UsuarioService.java
+++ clinica-back/clinica-back/src/main/java/clinica_back/clinica_back/features/Usuario/UsuarioService.java
@@ -2,6 +2,9 @@ package clinica_back.clinica_back.features.Usuario;
 
 import org.springframework.stereotype.Service;
 
+import clinica_back.clinica_back.features.Usuario.Endereco.Endereco;
+import clinica_back.clinica_back.features.Usuario.Endereco.dto.EnderecoRequestDTO;
+import clinica_back.clinica_back.features.Usuario.Endereco.dto.EnderecoResponseDTO;
 import clinica_back.clinica_back.features.Usuario.dto.UsuarioResponseDTO;
 import clinica_back.clinica_back.shared.exceptions.RecursoNaoEncontradoException;
 import lombok.RequiredArgsConstructor;
@@ -25,5 +28,26 @@ public class UsuarioService {
                 usuario.getEmail(),
                 usuario.getTelefone(),
                 usuario.getPerfil());
+    }
+
+    public EnderecoResponseDTO atualizarEndereco(Long idUsuario, EnderecoRequestDTO dto) {
+
+        Usuario usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(
+                        () -> new RecursoNaoEncontradoException("Usuário com ID " + idUsuario + " não encontrado!"));
+
+        Endereco endereco = usuario.getEndereco();
+
+        endereco.setRua(dto.getRua());
+        endereco.setNumero(dto.getNumero());
+        endereco.setBairro(dto.getBairro());
+        endereco.setCidade(dto.getCidade());
+        endereco.setEstado(dto.getEstado());
+        endereco.setCep(dto.getCep());
+
+        usuarioRepository.save(usuario);
+
+        return new EnderecoResponseDTO(endereco.getRua(), endereco.getNumero(), endereco.getBairro(),
+                endereco.getCidade(), endereco.getEstado(), endereco.getCep());
     }
 }
